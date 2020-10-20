148b4b473426f07f783b30056fe8a1f8e0a7d035
jubayer <mohammadjubayer999@gmail.com>
2020-10-21 00:36:33 +0100

    change front page ui

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,19 +1,56 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar  toggleable="lg" type="dark" variant="dark">
       <b-img src="/logo-white-new_7003.png" fluid alt="Right image"></b-img>
-       <b-button style="margin-left:50px;" pill variant="success" to="/" >
-                         Crypto Trivia</b-badge>
-                 </b-button>
+      <b-button style="margin-left: 40px" pill variant="success" to="/">
+        Crypto Trivia
+      </b-button>
+      <h5 class="balance">
+        <span class="iconB">B</span>1000h <a href="">(Buy)</a>
+      </h5>
+
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
+        <b-navbar-nav class="ml-auto d-flex">
+          <b-nav-item id="show-btn" @click="$bvModal.show('bv-modal-example')"
+          >Load Account</b-nav-item
+          >
           <b-nav-item to="/how">How to play</b-nav-item>
-          <b-nav-item to="/about">About the game</b-nav-item>
+          <b-nav-item to="/about"> About the game </b-nav-item>
           <b-nav-item to="/contact">Contact</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
   </div>
 </template>
+
+<style scoped>
+.balance {
+  position: absolute;
+  left: 85%;
+  bottom: 59%;
+  color: rgb(187, 187, 187);
+}
+.iconB {
+  font-weight: bold;
+  color: rgb(201, 200, 200);
+  margin: 0 0.5rem;
+}
+
+.buy:hover {
+  cursor: pointer;
+}
+
+@media screen and (max-width: 990px) {
+  .balance {
+    position: unset;
+  }
+}
+
+@media screen and (max-width: 400px) {
+  .balance {
+    margin: 1rem;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,22 +11,38 @@
         </div>
         <div>
           <div class="mt-3">
-              <b-alert
-                :show="dismissCountDown"
-                dismissible
-                fade
-                variant="warning"
-                @dismiss-count-down="countDownChanged"
-              > Please choose below category for playing quiz.
-              </b-alert>
-              <b-button size="lg" variant="success" @click="five" v-b-tooltip.hover="'Entry Fee 5ħ!'">Win 500ħ</b-button>
-              <b-button size="lg" variant="warning" @click="ten" v-b-tooltip.hover="'Entry Fee 10ħ!'">Win 1000ħ</b-button>
-
+            <b-alert
+              :show="dismissCountDown"
+              dismissible
+              fade
+              variant="warning"
+              @dismiss-count-down="countDownChanged"
+            >
+              Please choose below category for playing quiz.
+            </b-alert>
+            <b-button
+              id="buttonFee1"
+              class="buttonFee"
+              size="lg"
+              variant="success"
+              @click="five"
+              v-b-tooltip.hover="'Entry Fee 5ħ!'"
+            >Win 500ħ</b-button
+            >
+            <b-button
+              id="buttonFee2"
+              class="buttonFee"
+              size="lg"
+              variant="warning"
+              @click="ten"
+              v-b-tooltip.hover="'Entry Fee 10ħ!'"
+            >Win 1000ħ</b-button
+            >
           </div>
         </div>
         <div class="account">
           <div>
-            <b-button id="show-btn" variant="outline-primary" @click="$bvModal.show('bv-modal-example')">Load Account</b-button>
+          <!--  <b-button id="show-btn" variant="outline-primary" @click="$bvModal.show('bv-modal-example')">Load Account</b-button>-->
 
             <b-modal id="bv-modal-example" title="Load Your Hedera Account" hide-footer>
               <b-form-group label="Account Id" label-for="dropdown-form-email" @submit.stop.prevent>
@@ -51,11 +67,11 @@
               <b-button class="mt-3" block @click="onClickLock">Load</b-button>
             </b-modal>
           </div>
-          <h3>Your account current balance</h3>
+          <!--<h3>Your account current balance</h3>
           <h2>{{bal}} ħ</h2>
           <p>
             <a href="https://buy.moonpay.io/hbar">click here</a> to get some HBAR into your hedera wallet
-          </p>
+          </p>-->
 
         </div>
       </div>
@@ -179,19 +195,32 @@ export default {
 </script>
 
 <style scoped>
-.content {
-  background-color: blanchedalmond;
-}
-body, html {
+body,
+html {
   height: 100%;
   margin: 0;
 }
+#art {
+  color: white;
+  margin: 2rem 0;
+}
+.buttonFee {
+  border-radius: 50%;
+  height: 15rem;
+  width: 15rem;
+  margin: 1rem;
+  font-size: 2rem;
+  font-weight: bold;
+  color: white;
+  box-shadow: 5px 10px 8px #302c2c;
+}
 .content {
   /* The image used */
-  background-image: url('~@/assets/hh-1.jpg');
+  background-image: url("~@/assets/hh-1.jpg");
+  background-color: blanchedalmond;
 
   /* Full height */
-  height: 50%;
+  height: 100vh;
 
   /* Center and scale the image nicely */
   background-position: center;
@@ -214,6 +243,39 @@ body, html {
 .account {
   position: relative;
   top: 200px;
+}
+@media screen and (max-width: 550px) {
+  .buttonFee {
+    height: 10rem;
+    width: 10rem;
+    margin: 1rem 0;
+    font-size: 1.4rem;
+  }
+}
+@media screen and (max-width: 1150px) {
+  .content {
+    height: 150vh;
+  }
+}
+#buttonFee1:hover,
+#buttonFee1:hover ~ .bg {
+  opacity: 1;
+  background: #632c65;
+  background: -moz-linear-gradient(-45deg, #632c65 15%, #56a5e2 100%);
+  background: -webkit-linear-gradient(-45deg, #632c65 15%, #56a5e2 100%);
+  background: linear-gradient(135deg, #632c65 15%, #56a5e2 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#632c65', endColorstr='#56a5e2',GradientType=1 );
+  transform: scale(1.1);
+}
+#buttonFee2:hover,
+#buttonFee2:hover ~ .bg {
+  opacity: 1;
+  background: #e2a9e5;
+  background: -moz-linear-gradient(-45deg, #e2a9e5 15%, #2b94e5 100%);
+  background: -webkit-linear-gradient(-45deg, #e2a9e5 15%, #2b94e5 100%);
+  background: linear-gradient(135deg, #e2a9e5 15%, #2b94e5 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#e2a9e5', endColorstr='#2b94e5',GradientType=1 );
+  transform: scale(1.1);
 }
 h2 {
   border: 3px solid green;
