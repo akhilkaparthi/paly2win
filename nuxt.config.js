dbf3d955b197c4f00945488a01041583b66b333f
akhil kaparthi <akhilkaparthi@akhils-MacBook-Pro.local>
2020-10-22 14:20:50 +0100

    removed headers code in node.json file

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,21 @@ module.exports = {
   proxy: {
   '/api/hedera/five': {
     target: 'https://crypto-trivia.vercel.app/', changeOrigin: false
-  }
+  },
+  async headers() {
+      return [
+        {
+          // mathching all API routes
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+    }
   /*
    ** Build configuration
    */
