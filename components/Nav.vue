4e24e8cf2d299189603ddb934490911695e9f0f0
akhil kaparthi <akhilkaparthi@akhils-MacBook-Pro.local>
2020-10-23 13:51:12 +0100

    changes done in home page

--- components/Nav.vue
+++ components/Nav.vue
@@ -5,20 +5,24 @@
       <b-button style="margin-left: 40px" pill variant="success" to="/">
         Crypto Trivia
       </b-button>
+      <!--
       <h5 class="balance">
-        <span class="iconB"></span>Balance: {{ loading }}  <a href="https://buy.moonpay.io/hbar">(Buy)</a>
+        <span class="iconB"></span>Account: {{ loading }}  <a href="https://buy.moonpay.io/hbar">(Buy)</a>
       </h5>
-
+      -->
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto d-flex">
-          <b-nav-item id="show-btn" @click="$bvModal.show('bv-modal-example')"
-          >Load Account</b-nav-item
-          >
-          <b-nav-item to="/how">How to play</b-nav-item>
-          <b-nav-item to="/about"> About the game </b-nav-item>
-          <b-nav-item to="/contact">Contact</b-nav-item>
+          <b-nav-item to="/" v-b-tooltip.hover.bottom="'Click Here to Home Page!'">Home</b-nav-item>
+          <b-nav-item id="show-btn" v-b-tooltip.hover.bottom="'Click Here to load ħ bars!'" @click="$bvModal.show('bv-modal-example')">Load Account</b-nav-item>
+          <b-nav-item to="/how" v-b-tooltip.hover.bottom="'Click Here to how to play!'">How to play</b-nav-item>
+          <b-nav-item to="/about" v-b-tooltip.hover.bottom="'Click Here to about the game!'"> About the game </b-nav-item>
+          <b-nav-item to="/contact" v-b-tooltip.hover.bottom="'Click Here to view contact details!'">Contact</b-nav-item>
+          <b-nav-item-dropdown text="Account" v-b-tooltip.hover.bottom="'Click Here to Know account details'" right>
+                <b-dropdown-item variant="dark" v-b-tooltip.hover.left="'Current ħ bars!'" href="#" disabled>Balance : {{ loading }} ħ bars </b-dropdown-item>
+                <b-dropdown-item variant="primary" v-b-tooltip.hover.left="'Click Here to buy ħ bars!'" href="https://buy.moonpay.io/hbar">Buy : ħ Bars</b-dropdown-item>
+           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
