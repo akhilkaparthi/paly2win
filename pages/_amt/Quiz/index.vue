c9401309be50d3d240d9eea3312db4a7865b6e4d
akhil kaparthi <akhilkaparthi@akhils-MacBook-Pro.local>
2020-10-26 23:04:23 +0000

    questions url changed to EC2 instance

--- pages/_amt/Quiz/index.vue
+++ pages/_amt/Quiz/index.vue
@@ -82,7 +82,7 @@ export default {
         .then((res) => {
           if (res.data.status == "SUCCESS") {
             fetch(
-              "https://9b7e72701fee.ngrok.io/questions?amount=10",
+              "http://ec2-3-8-188-72.eu-west-2.compute.amazonaws.com:8080/questions?amount=10",
               {
                 method: "get",
               }
